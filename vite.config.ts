36778b2fe591d932dfcc09a66c6c835a14510412
Tomasz Chmielarz <tchmielarz1@gmail.com>
2026-02-06 00:02:12 +0100

    chore: add vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -18,5 +19,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "node",
   },
 });
